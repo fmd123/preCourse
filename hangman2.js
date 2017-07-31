bb1400865ac398ef86ff8e5b7bd98eba61fe0c30
Francesca Davis <fmdavis6@gmail.com>
2017-07-30 21:44:08 -0600

    fixed some issues and added a play again feature
    fixed the problem of the the player update message after player quits.
    enclosed in a playGame function so that I could have a play again feature.

--- hangman2.js
+++ hangman2.js
@@ -1,18 +1,20 @@
-var words = ["platypus", "superb", "xerox", "synergy", "apple", "claxon", "mellifluous", "metonymy", "adirondack", "adipose", "bulbous", "crepuscular", "hijab", "harmonious", "horticulture"];
+var wordsHard = ["platypus", "superb", "xerox", "synergy", "apple", "claxon", "mellifluous", "metonymy", "adirondack", "adipose", "bulbous", "crepuscular", "hijab", "harmonious", "horticulture"];
+
+
+var wordsEasy = ["cat", "dog", "cow", "pig", "goat", "snail"];
 var word = "";
 var answerArr =[];
 var guessString = "";
-var remainingLetters= word.length;
-var howMany = 0;
-var turns = word.length + 10;
 var guess  = "";
+var stop = false;
+var turns = 0;
 
 
 
 
 
 function pickWord(arr){
-	word = words[Math.floor(Math.random()*words.length)];
+	word = arr[Math.floor(Math.random()* arr.length)];
 	console.log(word);
 	return (word);
 	
@@ -33,24 +35,52 @@ function setUpAnswerArray(str){
 //ask for a letter
 function getGuess(){
 	guess = prompt("Please choose a letter or click cancel to stop playing");
+	if (guess === null){
+			remainingLetters = 0;
+			word = "";
+			answerArr =[];
+			guessString = "";
+			
+			turns = 0;
+			stop = true;
+			alert("goodbye!");
+			return;
+		}else if (guess.length !== 1){
+			console.log("Please guess a single letter!");
+		}else{
 	console.log (guess);
-	console.log(typeof guess);
 	return guess;
 }
+}
+/*-----------------------------------------------------------------------------------------*/
+//guess string shows all the letters you have guessed so far
+function updateGuessString(guess){
 
-
-
-function turnsLeft(turns){
-	turns -=1;
-	console.log (turns);
-	return turns;
+	guessString += guess + ",";
+	console.log("inside updateGuessString: " + guessString);
+	return guessString;
+}
+/*---------------------------------------------------------------------------------------*/
+//comparing guess to answerArr and filling in the answerArr
+function updateGameState(guess, word, answerArr) {
+	console.log("selected word is", word);
+	for(var j=0; j<word.length; j++){
+		if(guess === word[j]){
+			answerArr[j] = guess;
+		}
+	}
+	return answerArr;
 }
 
-//need to add turns left. Later.
-function showPlayerProgress(answerArr, guessString, remainingLetters, turns){
+
+/*-------------------------------------------------------------------------------------*/
+function showPlayerProgress(answerArr, guessString, remainingLetters){
+
+	turns--;
+	
 	for(var x=0; x<answerArr.length; x++){
-		if (answerArr[x]==="_"){
-			remainingLetters--;
+		if (answerArr[x] !=="_"){
+			remainingLetters = remainingLetters -1;
 		}
  	}
  	if (turns ===0 && remainingLetters >0){
@@ -61,65 +91,89 @@ function showPlayerProgress(answerArr, guessString, remainingLetters, turns){
  		congrats();
  		return;
  	}else {
- 	console.log(answerArr + " Letters you have guessed: " + guessString + " You have: " + remainingLetters + " to go and " +turns + " guesses left.");
+ 	alert(answerArr + " Letters you have guessed: " + guessString + " You have: " + remainingLetters + " to go and " +turns + " guesses left.");
  	return(remainingLetters);
  	}
 }
-
-
-
-
-//comparing guess to answerArr and filling in the answerArr
-function updateGameState(guess, word, answerArr) {
-	for(var j=0; j<word.length; j++){
-		if(guess === word[j]){
-			answerArr[j] = guess;
-		}
-	}
-	return answerArr;
-}
-
-
-//guess string shows all the letters you have guessed so far
-
-function updateGuessString(guess){
-
-	guessString += guess + ",";
-	console.log(guessString);
-	return guessString;
-}
-
-//the game loop function
-//function playGame(remainingLetters, turns, word, answerArr){
-	debugger;
-	pickWord(words);
-	setUpAnswerArray(word);
-	remainingLetters = word.length;
-	console.log(remainingLetters);
-	while(remainingLetters > 0 && turns >0){
-			getGuess();
-			updateGuessString(guess);
-			updateGameState(guess, word, answerArr);
-			showPlayerProgress(answerArr, guessString);
-			turnsLeft();
-			updateGameState();
-		}
-//}
-
+/*------------------------------------------------------------------------------*/
 //call either of these from within updateGameState and end game
 function congrats(){
-alert("you won");
+var again = prompt("you won. Do you want to play again? Enter any string into box or hit cancel to quit.");
+	if (again === null){
+		alert("goodbye!");
+		remainingLetters = 0;
+		word = "";
+		answerArr =[];
+		guessString = "";
+		guess  = "";
+		turns = 0;
+		stop = true;
+		return;
+		
+	}else{
+		alert("great!");
+		remainingLetters = 0;
+		word = "";
+		answerArr =[];
+		guessString = "";
+		guess  = "";
+		turns = 0;
+		stop = false;
+		
+		playGame();
+
+		
+	}
+
+return;
 }
 
 function lost(){
-alert("you lost");
+var again = prompt("you lost. Do you want to play again? Enter any string into box or hit cancel to quit.");
+	if (again === null){
+		alert("goodbye!");
+		remainingLetters = 0;
+		word = "";
+		answerArr =[];
+		guessString = "";
+		guess  = "";
+		turns = 0;
+		stop = true;
+		return;
+	}else{
+		alert("great!");
+		remainingLetters = 0;
+		word = "";
+		answerArr =[];
+		guessString = "";
+		guess  = "";
+		turns = 0;
+		stop = false;
+		playGame();
+		
+	}
+return;
 }
 
-/*--------------------------------------------------
-pickWord(words);
-setUpAnswerArray(word);
-remainingLetters = word.length;
-console.log(remainingLetters);
-*/
-//playGame(remainingLetters, turns, word, answerArr);
-/*----------------------------------------------------*/
+
+function playGame(){
+	//debugger;		
+/*-----------------------------------------------------------------------*/
+		pickWord(wordsHard);
+		setUpAnswerArray(word);
+		remainingLetters = word.length;
+		turns = word.length + 5;
+		
+		while(remainingLetters > 0 && turns >0 && stop === false){
+				//debugger;
+				getGuess();
+				if(guess !== null){
+				updateGuessString(guess);
+				updateGameState(guess, word, answerArr);
+				showPlayerProgress(answerArr, guessString, remainingLetters, turns);
+				}
+		}
+
+}
+playGame();
+
